568a604b1509964f1b15bbac06dc65710d2c2a73
Benjamin Streb <Bstreb@gmail.com>
2013-05-11 17:29:19 -0400

    More formatting.

--- argparser.h
+++ argparser.h
@@ -22,82 +22,69 @@ public:
     // parse the command line arguments
     for (int i = 1; i < argc; i++) {
       if (!strcmp(argv[i],"-input") ||
-	  !strcmp(argv[i],"-i")) {
-	i++; assert (i < argc);
-	input_file = std::string(argv[i]);
+          !strcmp(argv[i],"-i")) {
+        i++; assert (i < argc);
+        input_file = std::string(argv[i]);
       } else if (!strcmp(argv[i],"-vertex") ||
-		 !strcmp(argv[i],"-v")) {
-	i++; assert (i < argc); 
-	vertex_shader_filename = std::string(argv[i]);
+           !strcmp(argv[i],"-v")) {
+        i++; assert (i < argc); 
+        vertex_shader_filename = std::string(argv[i]);
       } else if (!strcmp(argv[i],"-fragment") ||
-		 !strcmp(argv[i],"-f")) {
-	i++; assert (i < argc); 
-	fragment_shader_filename = std::string(argv[i]);
+           !strcmp(argv[i],"-f")) {
+        i++; assert (i < argc); 
+        fragment_shader_filename = std::string(argv[i]);
       } else if (!strcmp(argv[i],"-size")) {
-	i++; assert (i < argc); 
-	width = height = atoi(argv[i]);
+        i++; assert (i < argc); 
+        width = height = atoi(argv[i]);
       } else if (!strcmp(argv[i],"-eval")) {
-		using_evaluator = true;
-		i++; assert (i < argc);
-		int games = atoi(argv[i]);
-		i++; assert (i < argc);
-		int turns = atoi(argv[i]);
-		
-		evaluator = new AI_Monte_Carlo(0, games, turns);
+        using_evaluator = true;
+        i++; assert (i < argc);
+        int games = atoi(argv[i]);
+        i++; assert (i < argc);
+        int turns = atoi(argv[i]);
+    
+        evaluator = new AI_Monte_Carlo(0, games, turns);
       } else if (!strcmp(argv[i],"-no_gui")) {
-		gui = false;
+        gui = false;
       }
       else if (!strcmp(argv[i],"-ai_vs_ai")) {
-		assert(!using_ai);
-		ai_vs_ai = true;
-		
+        assert(!using_ai);
+        ai_vs_ai = true;
       } else if (!strcmp(argv[i],"-ai_random")) {
-		if(!ai_vs_ai)
-		{
-			theAI = new AI_Random(1);
-			using_ai = true;
-		}
-		else
-		{
-			assert(ai_id < 2);
-			if(ai_id == 0)
-			{
-				theAI = new AI_Random(-1);
-				ai_id++;
-			}
-			else
-			{
-				theAI_2 = new AI_Random(1);
-				ai_id++;
-			}
-		}
+        if(!ai_vs_ai) {
+          theAI = new AI_Random(1);
+          using_ai = true;
+        } else {
+          assert(ai_id < 2);
+          if(ai_id == 0) {
+            theAI = new AI_Random(-1);
+            ai_id++;
+          } else {
+            theAI_2 = new AI_Random(1);
+            ai_id++;
+          }
+        }
       } else if (!strcmp(argv[i],"-ai_monte_carlo")) {
-		i++; assert (i < argc);
-		int games = atoi(argv[i]);
-		i++; assert (i < argc);
-		int turns = atoi(argv[i]);
-		if(!ai_vs_ai)
-		{
-			theAI = new AI_Monte_Carlo(1, games, turns);
-			using_ai = true;			
-		}
-		else
-		{
-			assert(ai_id < 2);
-			if(ai_id == 0)
-			{
-				theAI = new AI_Monte_Carlo(-1, games, turns);
-				ai_id++;
-			}
-			else
-			{
-				theAI_2 = new AI_Monte_Carlo(1, games, turns);
-				ai_id++;
-			}
-		}
+        i++; assert (i < argc);
+        int games = atoi(argv[i]);
+        i++; assert (i < argc);
+        int turns = atoi(argv[i]);
+        if(!ai_vs_ai) {
+          theAI = new AI_Monte_Carlo(1, games, turns);
+          using_ai = true;      
+        } else {
+          assert(ai_id < 2);
+          if(ai_id == 0) {
+            theAI = new AI_Monte_Carlo(-1, games, turns);
+            ai_id++;
+          } else {
+            theAI_2 = new AI_Monte_Carlo(1, games, turns);
+            ai_id++;
+          }
+        }
       } else {
-	printf ("whoops error with command line argument %d: '%s'\n",i,argv[i]);
-	assert(0);
+        printf ("whoops error with command line argument %d: '%s'\n",i,argv[i]);
+        assert(0);
       }
     }
   }
